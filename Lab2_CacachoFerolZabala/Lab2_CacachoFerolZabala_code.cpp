93b5ff158cd493ed524101b5a2714b4684ea0142
jeanmaxcacacho <maxcacacho@gmail.com>
2025-01-30 11:11:37 +0800

    added error tests for non-integer inputs and N mismatch

--- Lab2_CacachoFerolZabala/Lab2_CacachoFerolZabala_code.cpp
+++ Lab2_CacachoFerolZabala/Lab2_CacachoFerolZabala_code.cpp
@@ -7,15 +7,30 @@ int main() {
     int numLines;
     cin >> numLines;
 
+    if (cin.fail()) {
+        cout << "Error: First line in input text file is not an integer!" << endl;
+        return 1;
+    }
+
     for (int currentLine = 1; currentLine <= numLines; currentLine++) {
         int x, y;
         string str;
 
         cin >> x >> y;
-        getline(cin >> ws, str);
 
+        if (cin.fail()) {
+            cout << "Error: Invalid input detected, not both agent coordinates are integers!" << endl;
+            return 1;
+        }
+
+        getline(cin >> ws, str);
         cout << "Agent#" << currentLine << " is at (" <<  x << ", " << y << ")" << endl;
         cout << "Says: " << str << endl;
+    }
+
+    if (!(cin.eof())) {
+        cout << "Error: Amount of lines below the first line does not match N!" << endl;
+        return 1;
     }
 
     return 0;
