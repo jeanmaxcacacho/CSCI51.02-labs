1c02d19ec5a3037558f883879ceb441ad64ba5c0
jeanmaxcacacho <maxcacacho@gmail.com>
2025-02-01 23:13:33 +0800

    initial test

--- Lab3_CacachoFerolZabala/Lab3_CacachoFerolZabala_code.cpp
+++ Lab3_CacachoFerolZabala/Lab3_CacachoFerolZabala_code.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 
 int main(void) {
-    cout << "Hello World! \n";
+    IntStack intStack = IntStack();
+    intStack.push(6);
+    intStack.push(5);
+    intStack.push(4);
+    intStack.push(3);
+    intStack.push(2);
+    intStack.push(1);
+
+    // expected here is 1
+    cout << "The stack's current top is '" << intStack.top->data << "'." << endl;
+
+    intStack.pop();
+    intStack.pop();
+    // expected here is 3
+    cout << "The stack's current top is '" << intStack.top->data << "'." << endl;
+    // expected here is still 3
+    cout << "The stack's current top is '" << intStack.peek() << "'." << endl;
     return 0;
+    // 3 4 5 6 will get printed to console because of the destructor
 }
