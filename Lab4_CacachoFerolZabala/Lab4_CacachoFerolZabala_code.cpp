683fa2fa15010c483fe8ab1c5d9db7ec44757e66
jeanmaxcacacho <maxcacacho@gmail.com>
2025-02-19 16:09:14 +0800

    implementation of russian peasant algorithm

--- Lab4_CacachoFerolZabala/Lab4_CacachoFerolZabala_code.cpp
+++ Lab4_CacachoFerolZabala/Lab4_CacachoFerolZabala_code.cpp
@@ -2,36 +2,40 @@
 
 using namespace std;
 
-int manMult(int x, int y);
+int russianPeasant(int x, int y);
 
 int main(void) {
-    cout << "2 and 3: " << manMult(2, 3) << endl;
-    cout << "12 and 12: " << manMult(12, 12) << endl;
-    cout << "9 and 4: " << manMult(9, 4) << endl;
-    cout << "-2 and 5: " << manMult(-2, 5) << endl;
-    cout << "-2 and -14: " << manMult(-2, -14) << endl;
+    cout << "Product of 2 and 3: " << russianPeasant(2, 3) << endl;
+    cout << "Product of 12 and 12: " << russianPeasant(12, 12) << endl;
+    cout << "Product of 9 and 4: " << russianPeasant(9, 4) << endl;
+    cout << "Product of -2 and 5: " << russianPeasant(-2, 5) << endl;
+    cout << "Product of -2 and -14: " << russianPeasant(-2, -14) << endl;
+    cout << "Product of -9 and 100: " << russianPeasant(-9, 100) << endl;
 
-    // expected to see 6, 144, 36, -10, and 28 respectively
-
+    // expected values are 6, 144, 36, -10, 28, -900
     return 0;
 }
 
-int manMult(int x, int y) {
+// https://www.youtube.com/watch?v=N_RC0skbReU
+int russianPeasant(int x, int y) {
+    if (x == 0 || y == 0) return 0;
+
     bool isNegative = (x < 0) ^ (y < 0);
 
-    x = abs(x);
-    y = abs(y);
+    if (x < 0) x = -x;
+    if (y < 0) y = -y;
 
     int product = 0;
-    int current_x = x;
 
     while (y > 0) {
         if (y & 1) {
-            product += current_x;
+            product += x;
         }
-        current_x <<= 1;
+        x <<= 1;
         y >>= 1;
     }
 
-    return isNegative ? -product : product;
+    if (isNegative) product = -product;
+
+    return product;
 }
