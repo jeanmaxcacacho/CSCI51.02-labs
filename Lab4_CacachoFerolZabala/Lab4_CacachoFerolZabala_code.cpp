aaa70d8d45d6d10cee2353d50b3c0bec3ddba10a
jeanmaxcacacho <maxcacacho@gmail.com>
2025-02-23 21:02:02 +0800

    reformatted code, removed redundant test cases

--- Lab4_CacachoFerolZabala/Lab4_CacachoFerolZabala_code.cpp
+++ Lab4_CacachoFerolZabala/Lab4_CacachoFerolZabala_code.cpp
@@ -5,13 +5,6 @@ using namespace std;
 int russianPeasant(int x, int y);
 
 int main(void) {
-    // cout << "Product of 2 and 3: " << russianPeasant(2, 3) << endl;
-    // cout << "Product of 12 and 12: " << russianPeasant(12, 12) << endl;
-    // cout << "Product of 9 and 4: " << russianPeasant(9, 4) << endl;
-    // cout << "Product of -2 and 5: " << russianPeasant(-2, 5) << endl;
-    // cout << "Product of -2 and -14: " << russianPeasant(-2, -14) << endl;
-    // cout << "Product of -9 and 100: " << russianPeasant(-9, 100) << endl;
-
     int x = 7;
 
     cout << "Product of 7 and 45: " << russianPeasant(x, 45) << endl;
@@ -22,7 +15,6 @@ int main(void) {
     return 0;
 }
 
-// https://www.youtube.com/watch?v=N_RC0skbReU
 int russianPeasant(int x, int y) {
     if (x == 0 || y == 0) return 0;
 
