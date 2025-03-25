851778d2d52d096a5a8e69188d7a7879b4c2a933
jeanmaxcacacho <maxcacacho@gmail.com>
2025-03-25 22:30:51 +0800

    clock works, purely with ctime

--- Lab6_CacachoFerolZabala/Lab6_CacachoFerolZabala_code.cpp
+++ Lab6_CacachoFerolZabala/Lab6_CacachoFerolZabala_code.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <unistd.h>
+#include <unistd.h> // fork() and sleep() come from here
+#include <ctime>
 
 using namespace std;
 
@@ -15,7 +16,21 @@ int main(int argc, char* argv[]) {
             return -1;
         }
     } else {
-        cout << "I am your parent nigga!" << endl;
+        int counter = 1; // start at 1st print-out
+
+        while (true) {
+            time_t now = time(nullptr);
+            tm *t = localtime(&now);
+            printf("[%04d-%02d-%02d] %02d:%02d:%02d\n",
+                   t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+                   t->tm_hour, t->tm_min, t->tm_sec);
+            if (counter == 3) {
+                counter == 0;
+                printf("\"This program has gone on for far too long. Type Ctrl+C to abort this timer application.\"\n");
+            }
+            counter++;
+            sleep(10);
+        }
     }
 
     return 0;
